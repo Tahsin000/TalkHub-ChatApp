1ea2cc922e9d875c699aee3ac969b3c06c86942e
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2023-08-13 23:44:59 +0600

    Setup, Layout, Pages & Routes

---
+++ TalkHub/src/layouts/MainLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">
+          {{ title }}
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+export default {
+  setup () {
+    const route = useRoute()
+    const title = ref('')
+    const updatePageTitle = () => {
+      if (route.fullPath === '/') title.value = 'TalkHub'
+      else if (route.fullPath === '/chat') title.value = 'Chat'
+      else if (route.fullPath === '/auth') title.value = 'Auth'
+    }
+    onMounted(updatePageTitle)
+
+    watch(() => route.fullPath, updatePageTitle)
+    return {
+      title
+    }
+  }
+}
+</script>
+<style lang="">
+</style>
---
+++ TalkHub/src/pages/PageUsers.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex">
+    <h5>User Page</h5>
+  </q-page>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
