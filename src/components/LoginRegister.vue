b226879387448ad62b190c7338b6295466e182e4
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2023-08-16 01:11:07 +0600

    Login / Register Page

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -1,12 +1,37 @@
 <template lang="">
-    <q-form>
-        <q-input outlined v-model="text" label="Label" :dense="dense" />
+    <q-form @submit="submitForm">
+        <q-input v-if="tab=='register'" class="q-mb-md" outlined v-model="userData.name" label="Name" :dense="dense" />
+        <q-input class="q-mb-md" outlined v-model="userData.email" label="Email" :dense="dense" />
+        <q-input class="q-mb-md" outlined v-model="userData.name" label="Name" :dense="dense" />
+        <div class="row">
+            <q-space></q-space>
+            <q-btn type="submit" color="primary" :label="tab"></q-btn>
+        </div>
     </q-form>
 </template>
 <script>
+import { ref } from 'vue'
 export default {
   props: {
     tab: String
+  },
+  setup (props) {
+    const tab = props.tab
+    const userData = ref({
+      name: '',
+      email: '',
+      password: ''
+    })
+    const submitForm = () => {
+      if (tab === 'login') {
+        console.log('Login the user')
+      } else {
+        console.log('register the user')
+      }
+    }
+    return {
+      userData, submitForm
+    }
   }
 }
 </script>
