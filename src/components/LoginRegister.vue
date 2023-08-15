21e3a2d5f7ab23f39c141a915f963d7f741c01b0
Tahsin000 <59111015+Tahsin000@users.noreply.github.com>
2023-08-15 17:45:19 +0600

    5:45 Pm

---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <q-form>
+        <q-input outlined v-model="text" label="Label" :dense="dense" />
+    </q-form>
+</template>
+<script>
+export default {
+  props: {
+    tab: String
+  }
+}
+</script>
+<style lang="">
+</style>
