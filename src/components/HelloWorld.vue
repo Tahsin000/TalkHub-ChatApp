fbe28343909d85706ebb27d92a113dfb9151016a
Tahsin000 <towahatahsin01@gmail.com>
2023-08-22 01:11:38 +0600

    startup - vueX

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <p class="display_100">count is {{ count }}</p>
+    <div class="_flex">
+      <button type="button" @click="count--">-</button>
+      <button type="button" @click="count++">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { ref } from "vue";
+export default {
+  props:{
+    msg: String
+  },
+  setup(){
+  
+  const count = ref(0);
+    return{
+      count
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+._flex {
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+}
+.display_100 {
+  font-size: 3rem;
+  font-weight: 300;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import store from './store' // Import your Vuex store
 
-const app = createApp(App)
-app.use(store) // Use the Vuex store
-
-app.mount('#app')
+createApp(App).mount('#app')
